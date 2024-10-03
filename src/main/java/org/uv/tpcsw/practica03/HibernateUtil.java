16a0a844fbe7e467c4a92102b804d10c773e73ae
habacucsoto <josehabacuc182020@gmail.com>
2024-10-03 13:01:54 -0600

    habacuc's commit 3-10-24

--- src/main/java/org/uv/tpcsw/practica03/HibernateUtil.java
+++ src/main/java/org/uv/tpcsw/practica03/HibernateUtil.java
@@ -6,21 +6,24 @@ import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
     
-    private static final SessionFactory sessionFactory;
- 
-     static {
-        // Create the SessionFactory from standard (hibernate.cfg.xml) 
-        // config file.
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static SessionFactory buildSessionFactory(){
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch(Throwable ex) {
+            System.err.println("SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
     }
+    
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+ 
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+    
     
 }
